090a2bb5496bf4bf23d119caa559fabd83851956
yuralala8 <yc1435@nyu.edu>
2017-06-19 17:38:29 -0400

    nesting commit

--- nested.rb
+++ nested.rb
@@ -15,12 +15,12 @@ def hopper
           :languages => ["C"]
         }
      }
-
-
+    
+    return programmer_hash[:grace_hopper]
+    
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+        
+        return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +57,18 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+        return programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 	programmer_hash = 
  		{
+            :yukihiro_matsumoto => {
+                :known_for => "Ruby",
+                :languages => ["LISP, C"]
+            },
+            
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -99,9 +102,10 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     
+     return programmer_hash
      
 end
 
@@ -122,4 +126,8 @@ def adding_to_dennis
         }
      }
 
+    programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+    
+    return programmer_hash
+    
 end
